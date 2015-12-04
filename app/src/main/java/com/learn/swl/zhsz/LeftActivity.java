90a5efd3baebac741e6be397fe27ddcc8e35d247
swl <443770865@qq.com>
2015-12-04 23:13:51 +0800

    修改2

--- app/src/main/java/com/learn/swl/zhsz/LeftActivity.java
+++ app/src/main/java/com/learn/swl/zhsz/LeftActivity.java
@@ -1,22 +1,26 @@
 package com.learn.swl.zhsz;
 
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import com.learn.swl.zhsz.Fragment.SettingFragment;
 
 public class LeftActivity extends FragmentActivity {
-
+    private Fragment[] fragments ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_left);
+        int position = getIntent().getIntExtra("position", 0);
+        initFragment(position);
+    }
+    private void initFragment(int position){
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction= fm.beginTransaction();
         transaction.replace(R.id.layout_left_activity,new SettingFragment());
         transaction.commit();
     }
-
 
 }
